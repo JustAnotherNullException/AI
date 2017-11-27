41e7c94f7541518b20a1ab3a582e378dd394be7a
JustAnotherNullException <32852918+JustAnotherNullException@users.noreply.github.com>
2017-11-27 13:28:59 +0000

    Added Agent

--- GeneticAlgorithms/Assets/Scripts/Grid.cs
+++ GeneticAlgorithms/Assets/Scripts/Grid.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-
+    public GameObject Agent;
     public Transform TilePrefab;
     public Vector2 mapSize;
 
@@ -21,7 +21,8 @@ public class Grid : MonoBehaviour
         
     }
 
-    public void GenerateGrid() {
+    public void GenerateGrid()
+    {
 
 
         string holderName = "GeneratedGrid";
@@ -31,23 +32,46 @@ public class Grid : MonoBehaviour
             DestroyImmediate(transform.Find(holderName).gameObject);
         }
 
-        GameObject[,] Tiles = new GameObject[mapSize.x ,mapSize.y];
+        GameObject[,] Tiles = new GameObject[(int)mapSize.x, (int)mapSize.y];
 
         Transform gridHolder = new GameObject(holderName).transform;
         gridHolder.parent = transform;
 
-        for (int x = 0; x < mapSize.x; x++) {
+        for (int x = 0; x < mapSize.x; x++)
+        {
 
-            for (int y = 0; y < mapSize.y; y++) {
-                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, - mapSize.y/2+ 0.5f + y);
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -(-mapSize.y / 2 + 0.5f + y));
                 Transform newTile = Instantiate(TilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
                 newTile.localScale = Vector3.one * (1 - outlinePercent);
                 newTile.parent = gridHolder;
 
-                Tiles[x, y] = newTile;
-            }   
+                Tiles[x, y] = newTile.gameObject;
+            }
         }
+        
+        Tiles[0, 0].SetColor(Color.green);
+        Tiles[9, 9].SetColor(Color.red);
 
-        Tiles[5, 3].SetActive(false);
+        Agent.MoveTo(Tiles[0, 0]);
+        Agent.SetColor(Color.black);
+    }
+}
+
+public static class Extensions
+{
+    public static void SetColor(this GameObject gameObj, Color color)
+    {
+        Renderer renderer = gameObj.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.color = color;
+        }
+    }
+
+    public static void MoveTo(this GameObject gameObject, GameObject target)
+    {
+        gameObject.transform.position = target.transform.position;
     }
 }
