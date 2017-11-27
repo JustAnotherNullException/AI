2ab8953b36da442c44981636dd0e99a202dbc779
JustAnotherNullException <32852918+JustAnotherNullException@users.noreply.github.com>
2017-11-27 12:34:57 +0000

    Updated Defualts

---
+++ GeneticAlgorithms/Assets/Scripts/Grid.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    public Transform TilePrefab;
+    public Vector2 mapSize;
+
+    [Range (0,1)]
+    public float outlinePercent;
+
+    void Start()
+    {
+        mapSize.x = 10;
+        mapSize.y = 10;
+        outlinePercent = 0.2f;
+
+        GenerateGrid();
+        
+    }
+
+    public void GenerateGrid() {
+
+
+        string holderName = "GeneratedGrid";
+
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform gridHolder = new GameObject(holderName).transform;
+        gridHolder.parent = transform;
+
+        for (int x = 0; x < mapSize.x; x++) {
+
+            for (int y = 0; y < mapSize.y; y++) {
+                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, - mapSize.y/2+ 0.5f + y);
+                Transform newTile = Instantiate(TilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outlinePercent);
+                newTile.parent = gridHolder;
+            }   
+        }
+    }
+}
