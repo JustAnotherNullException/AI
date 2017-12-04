0e25e06af34c875cf83ec2f47864ce48148911c0
JustAnotherNullException <32852918+JustAnotherNullException@users.noreply.github.com>
2017-12-04 12:27:10 +0000

    Randomally Finds Finish Line

--- GeneticAlgorithms/Assets/Scripts/GeneticAlgorithm.cs
+++ GeneticAlgorithms/Assets/Scripts/GeneticAlgorithm.cs
@@ -1,35 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GeneticAlgorithm : MonoBehaviour
 {
-    Agent A;
-    Agent B;
-    Agent C;
+    public Grid grid;
+
+    List<Agent> Pop;
+
+    TileSet[,] tileSet;
 
     private void Start()
     {
-        A = new Agent();
-        B = new Agent();
-        C = new Agent();
+
+        tileSet = grid.GenerateTileSet();
+        Pop = new List<Agent>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            Pop.Add (new Agent());
+        }
+
+        Debug.Log(Pop.Max(a => a.CalFitness(tileSet)));
+
+        /*int empties = 0;
+        int walls = 0;
+        int starts = 0;
+        int finishes = 0;
+        foreach (var tile in tileSet)
+        {
+            if (tile == TileSet.Empty) empties++;
+            else if (tile == TileSet.Wall) walls++;
+            else if (tile == TileSet.Start) starts++;
+            else if (tile == TileSet.Finish) finishes++;
+        }
+
+        Debug.Log("empties = " + empties);
+        Debug.Log("walls = " + walls);
+        Debug.Log("starts = " + starts);
+        Debug.Log("finishes = " + finishes);*/
+
     }
 
     private void Update()
     {
-        DrawDebugPath(A, Color.green);
-        DrawDebugPath(B, Color.black);
-        DrawDebugPath(C, Color.white);
+        DrawDebugPath(Pop.OrderByDescending(a => a.CalFitness(tileSet)).First(), Color.magenta);
     }
 
     private void DrawDebugPath(Agent agent, Color color)
     {
         Vector3 lineStart = transform.position;
-        for (int i = 0; i < agent.Genes.Length; i++)
-        {
-            Action gene = agent.Genes[i];
 
-            Vector3 lineEnd = lineStart ;
+        int i = 0;
+        foreach (Action gene in agent.CalPath(tileSet))
+        {
+            Vector3 lineEnd = lineStart;
             if (gene == Action.Up) lineEnd.z += 1;
             else if (gene == Action.Down) lineEnd.z -= 1;
             else if (gene == Action.Left) lineEnd.x -= 1;
@@ -38,11 +64,11 @@ public class GeneticAlgorithm : MonoBehaviour
             Color actualColor = color;
             if (i == 0) actualColor = Color.blue;
             if (i == agent.Genes.Length - 1) actualColor =  Color.red;
-
-
+            
             Debug.DrawLine(lineStart, lineEnd, actualColor);
 
             lineStart = lineEnd;
+            i++;
         }
     }
 }
@@ -55,6 +81,7 @@ public enum Action
     Left,
     Right
 }
+
 
 public class Agent
 {
@@ -77,9 +104,116 @@ public class Agent
         }
     }
 
-    public int CalFitness()
+    public int CalFitness(TileSet[,] tileSet)
     {
-        return 0;
+        int posX = 0;
+        int posY = 0;
+
+        int ActionsTaken = 0;
+
+        GetStart(tileSet, out posX, out posY);
+
+        foreach(Action Gene in Genes)
+        {
+            if(Gene == Action.Up)
+            {
+                posY--;
+            }
+
+            if (Gene == Action.Down)
+            {
+                posY++;
+            }
+
+            if (Gene == Action.Right)
+            {
+                posX++;
+            }
+
+            if (Gene == Action.Left)
+            {
+                posX--;
+            }
+
+            ActionsTaken++;
+
+            if (tileSet[posX,posY] == TileSet.Wall) // If it steps on a wall KILL IT 
+            {
+                return ActionsTaken;
+            }
+
+            if(tileSet[posX, posY] == TileSet.Finish) // If it steps on the finish line Good
+            {
+                return 1000 + (maximumActions - ActionsTaken);
+            }
+        }
+
+        return 0; ;
     }
 
+    public List<Action> CalPath(TileSet[,] tileSet)
+    {
+        List<Action> actionsTaken = new List<Action>();
+
+        int posX = 0;
+        int posY = 0;
+
+        GetStart(tileSet, out posX, out posY);
+
+        foreach (Action Gene in Genes)
+        {
+            if (Gene == Action.Up)
+            {
+                posY--;
+            }
+
+            if (Gene == Action.Down)
+            {
+                posY++;
+            }
+
+            if (Gene == Action.Right)
+            {
+                posX++;
+            }
+
+            if (Gene == Action.Left)
+            {
+                posX--;
+            }
+
+            actionsTaken.Add(Gene);
+
+            if (tileSet[posX, posY] == TileSet.Wall) // If it steps on a wall KILL IT 
+            {
+                return actionsTaken;
+            }
+
+            if (tileSet[posX, posY] == TileSet.Finish) // If it steps on the finish line Good
+            {
+                return actionsTaken;
+            }
+        }
+
+        return actionsTaken;
+    }
+
+    private static void GetStart(TileSet[,] tileSet, out int posX, out int posY)
+    {
+        posX = 0;
+        posY = 0;
+
+        for (int x = 0; x < tileSet.GetLength(0); x++) // Gets X Co-ord for Start Position
+        {
+            for (int y = 0; y < tileSet.GetLength(1); y++) // Get Y Co-ord for Start Position
+            {
+                if (tileSet[x, y] == TileSet.Start)
+                {
+                    posX = x;
+                    posY = y;
+                }
+            }
+        }
+    }
 }
+
--- GeneticAlgorithms/Assets/Scripts/Grid.cs
+++ GeneticAlgorithms/Assets/Scripts/Grid.cs
@@ -8,23 +8,26 @@ public class Grid : MonoBehaviour
     public Transform TilePrefab;
     public Vector2 mapSize;
 
-    [Range (0,1)]
+    [Range(0, 1)]
     public float outlinePercent;
 
-    void Start()
+    GameObject[,] Tiles;
+
+    void Awake()
     {
         mapSize.x = 11;
         mapSize.y = 11;
         outlinePercent = 0.5f;
 
+        Tiles = new GameObject[(int)mapSize.x, (int)mapSize.y];
+
         GenerateGrid();
-        
+        GenerateWalls();
+
     }
 
     public void GenerateGrid()
     {
-
-
         string holderName = "GeneratedGrid";
 
         if (transform.Find(holderName))
@@ -32,14 +35,11 @@ public class Grid : MonoBehaviour
             DestroyImmediate(transform.Find(holderName).gameObject);
         }
 
-        GameObject[,] Tiles = new GameObject[(int)mapSize.x, (int)mapSize.y];
-
         Transform gridHolder = new GameObject(holderName).transform;
         gridHolder.parent = transform;
 
         for (int x = 0; x < mapSize.x; x++)
         {
-
             for (int y = 0; y < mapSize.y; y++)
             {
                 Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -(-mapSize.y / 2 + 0.5f + y));
@@ -50,13 +50,66 @@ public class Grid : MonoBehaviour
                 Tiles[x, y] = newTile.gameObject;
             }
         }
-        
+
         Tiles[1, 1].SetColor(Color.green);
         Tiles[9, 9].SetColor(Color.red);
 
         Agent.MoveTo(Tiles[1, 1]);
         Agent.SetColor(Color.black);
     }
+
+    public void GenerateWalls()
+    {
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            Tiles[x, 0].SetColor(Color.black);
+            Tiles[x, (int)mapSize.y - 1].SetColor(Color.black);
+        }
+        for (int y = 0; y < mapSize.y; y++)
+        {
+            Tiles[0, y].SetColor(Color.black);
+            Tiles[(int)mapSize.x - 1, y].SetColor(Color.black);
+        }
+    }
+
+    public TileSet[,] GenerateTileSet()
+    {
+        TileSet[,] tileSet = new TileSet[(int)mapSize.x, (int)mapSize.y];
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Color color = Tiles[x, y].GetColor();
+                if(color == Color.white)
+                {
+                    tileSet[x, y] = TileSet.Empty;
+                }
+                else if (color == Color.black)
+                {
+                    tileSet[x, y] = TileSet.Wall;
+                }
+                else if (color == Color.green)
+                {
+                    tileSet[x, y] = TileSet.Start;
+                }
+                else if (color == Color.red)
+                {
+                    tileSet[x, y] = TileSet.Finish;
+                }
+            }
+        }
+
+        return tileSet;
+    }
+}
+
+public enum TileSet
+{
+    Empty,
+    Wall,
+    Start,
+    Finish
 }
 
 public static class Extensions
@@ -68,6 +121,16 @@ public static class Extensions
         {
             renderer.material.color = color;
         }
+    }
+
+    public static Color GetColor(this GameObject gameObj)
+    {
+        Renderer renderer = gameObj.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            return renderer.material.color;
+        }
+        else return Color.white; 
     }
 
     public static void MoveTo(this GameObject gameObject, GameObject target)
